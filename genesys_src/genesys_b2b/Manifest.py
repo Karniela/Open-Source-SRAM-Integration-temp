d7212e0d5910dd1b46ba354cbb9a1aacb5cec548
Karniela <51403375+Karniela@users.noreply.github.com>
2024-12-07 21:57:29 -0800

    Add files via upload

---
+++ genesys_src/genesys_b2b/Manifest.py
@@ -0,0 +1,9 @@
+files = [
+    "genesys_top_module_b2b.v",
+    "controller_b2b.sv",
+    "performance_counters_b2b.sv",
+    "obuf_interface_b2b_neuroweaver.v",
+    "instruction_memory_wrapper_b2b.v",
+    "instruction_memory_b2b.v",
+    "decoder_b2b.v"
+]
